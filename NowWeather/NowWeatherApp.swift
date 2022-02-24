90ce44f4fe404f7acc45cae2dbeebb41f488d378
Wilson Leung <wilsonleung0914@gmail.com>
2022-02-25 00:07:15 +0800

    Initial Commit

---
+++ NowWeather/NowWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowWeatherApp.swift
+//  NowWeather
+//
+//  Created by Wilson Leung on 25/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct NowWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
